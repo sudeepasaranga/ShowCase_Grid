254e4110dfec27009e2d6f24164ab8edeac87e18
sudeepasaranga <sarangasadeepa@gmail.com>
2024-10-09 22:15:30 +0530

    Create Filter Button Component

---
+++ src/app/components/FilterButton.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function FilterButton({ text, active }) {
+  return (
+    <button
+      className={`gap-2.5 self-stretch px-5 py-2.5 rounded-[50px] ${
+        active ? 'bg-lime-50' : 'bg-white'
+      } border border-solid border-stone-900 border-opacity-20`}
+    >
+      {text}
+    </button>
+  );
+}
+
+export default FilterButton;
