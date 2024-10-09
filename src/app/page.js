fd50325143b4d2fe38b8ca1a0bf4047890fc1518
sudeepasaranga <sarangasadeepa@gmail.com>
2024-10-09 21:46:17 +0530

    Initial commit

---
+++ src/app/page.js
@@ -0,0 +1,13 @@
+// import CourseList from '@/app/components/CourseList'; 
+// import FilterButton from '@/app/components/FilterButton'; 
+
+const Page = () => {
+  return (
+    <main className="container mx-auto p-4">
+      {/* <FilterButton />
+      <CourseList /> */}
+    </main>
+  );
+};
+
+export default Page;
